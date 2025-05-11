10c530c125cd4878c976cffa90cb971cf5d8d8a4
Ajitkumar-1001 <sachinajitkumarp@gmail.com>
2025-05-11 07:30:47 -0400

    src push

--- src/config.py
+++ src/config.py
@@ -1,12 +1,14 @@
 import os
+import sys
 from pathlib import Path
-
 from dotenv import load_dotenv
 
+# Load environment variables
 load_dotenv()
 
 # Define the project root directory (Citibike_prediction_aml)
-PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+sys.path.append(str(PROJECT_ROOT))
 
 # Define directories relative to the project root
 DATA_DIR = PROJECT_ROOT / "data"
@@ -25,9 +27,11 @@ for directory in [
 ]:
     directory.mkdir(parents=True, exist_ok=True)
 
+# Environment variables
 HOPSWORKS_API_KEY = os.getenv("HOPSWORKS_API_KEY")
 HOPSWORKS_PROJECT_NAME = os.getenv("HOPSWORKS_PROJECT_NAME")
 
+# Constants for Hopsworks Feature Store and Model Registry
 FEATURE_GROUP_NAME = "time_series_hourly_feature_group_citi_bike"
 FEATURE_GROUP_VERSION = 1
 
